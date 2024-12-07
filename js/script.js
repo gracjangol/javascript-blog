26f4eff828b4d5a6a8823674539d48bf1e6c5b8b
Gracjan Gołębiewski <gra.golebiewski@gmail.com>
2024-12-07 07:52:30 +0100

    add removing active classes after post click

--- js/script.js
+++ js/script.js
@@ -5,9 +5,22 @@ function titleClickHandler(event){
 
   /* remove class 'active' from all article links  */
 
+  const activeLinks = document.querySelectorAll('.titles a.active');
+
+  for(let activeLink of activeLinks){
+  activeLink.classList.remove('active');
+  }
+
   /* add class 'active' to the clicked link */
 
   /* remove class 'active' from all articles */
+
+  const activePosts = document.querySelectorAll('.post.active');
+
+  for(let activePost of activePosts){
+  activePost.classList.remove('active');
+  }
+
 
   /* get 'href' attribute from the clicked link */
 

2838bfc06d83a72fd9496cea64bc2a4dd0ef89a6
Gracjan Gołębiewski <gra.golebiewski@gmail.com>
2024-12-09 07:18:13 +0100

    add dynamic title generation

--- js/script.js
+++ js/script.js
@@ -42,8 +42,51 @@ function titleClickHandler(event){
   activeArticle.classList.add('active')
 }
 
-const links = document.querySelectorAll('.titles a');
+const optArticleSelector = '.post',
+  optTitleSelector = '.post-title',
+  optTitleListSelector = '.titles';
 
-for(let link of links){
-  link.addEventListener('click', titleClickHandler);
+function generateTitleLinks(){
+
+  /* remove contents of titleList */
+  const titleList = document.querySelector(optTitleListSelector);
+  titleList.innerHTML = '';
+
+
+  /* for each article */
+  let html = '';
+
+  const articles = document.querySelectorAll(optArticleSelector)
+  for (let article of articles){
+
+    /* get the article id */
+
+    const articleId = article.getAttribute("id");
+
+    /* find the title element */
+
+    const articleTitle = article.querySelector(optTitleSelector).innerHTML;
+
+    /* get the title from the title element */
+
+
+    /* create HTML of the link */
+
+    const linkHTML = `<li><a href="#${articleId}"><span>${articleTitle}</span></a></li>`;
+    console.log(linkHTML)
+
+    /* insert link into titleList */
+    titleList.insertAdjacentHTML("beforeend",linkHTML)
+    html = html + linkHTML;
+    console.log(html)
+
+    const links = document.querySelectorAll('.titles a');
+
+    for(let link of links){
+    link.addEventListener('click', titleClickHandler);
+    }
+  }
+
 }
+
+generateTitleLinks();
